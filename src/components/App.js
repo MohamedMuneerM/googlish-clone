91c00411cd2ca554e7ef384a75f5959ab453a559
MohamedMuneerM <rehanamujeebh@gmail.com>
2021-02-08 21:36:37 +0530

    added h1 tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import SearchBar from './SearchBar/SearchBar'
-import SchdeluleList from './Schedule/ScheduleList'
-import { Button } from '@material-ui/core';
-import HideAppBar from './navbar/navbar.js'
-import SimplePaper from './paperComponent/paperBg'
 
 const App = () => {
 	return (
 		// <h1>hey</h1>
 		<div>
-			
-			<HideAppBar/>
-			<h5>Learn react</h5>
-			<SearchBar/>
-			<SchdeluleList/>
-			<SimplePaper/>
-			<p>lorem jagpegjhepgepgte
-			etgqotpqotuq[utw[utqwt
-			wqtwqptqptuqptuputt
-			twtjtjqpwtuqw[tuqwt
-			tqiwqptuqp[uq[wqtwqptqptuqptuputttqwtqpotqt
-			tqwttttttttttttttttttttttt
-			tttttttttttttttttttttttttt
-			ttttttttttttttttttttttttttt
-			tttttttttttttttttttttttttttttttttttttttttt
-
-			ttttttttttttttttttttttttttttttttttttttttttt</p>
-			<Button variant="contained" color="primary">Hello World</Button>
+			<h1>Google clone</h1>
 		</div>
 		
 	)
