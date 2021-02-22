10dc83e9f59a74b3ae5fb18e391cbf21cef044eb
MohamedMuneerM <rehanamujeebh@gmail.com>
2021-02-22 19:12:37 +0530

    improvements to the searchbar

--- src/components/GoogleSearchBar/GoogleSearchBar.js
+++ src/components/GoogleSearchBar/GoogleSearchBar.js
@@ -1,27 +1,47 @@
-import React from 'react'
+import {React, useState} from 'react'
 import {Container, Row, Col, Form, Button, FormControl } from 'react-bootstrap'
 import "./GoogleSearchBar.css"
 
 
 const GoogleSearchBar = () => {
+	const [searchTerm, setSearchTerm] = useState("Search...")
+
+	const updateValue = (e) => {
+		setSearchTerm(e.target.value)
+	}
+
+	const sendQueryText = (e) => {
+		e.preventDefault()
+		// fetch('127.0.0.1:800/api/', {method:"POST"})
+		// .then(response => console.log(response))
+		// .catch(error => console.log(error))
+		console.log('query send!! ' + searchTerm)
+	}
+
+
 	return (
 
 		<Container>
 
-			<Form>
-				<div style={{ "marginTop": "150px" }}>
+			<Form onSubmit={sendQueryText}>
+				<div style={{ "marginTop": "150px" }} className="wrapper mx-auto justify-content-center">
 					<div className="d-flex justify-content-center my-4 "><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google-logo"/></div>
 					
-					<div className="searchbar d-flex justify-content-center mx-auto">
+
+					<div className="searchbar d-flex flex-row justify-content align-items-center" >
+
+						<span><i className="fa fa-search"></i></span>
 						
-						<FormControl type="text" className="rounded-pill round-search">
+						<FormControl onChange={updateValue} value={searchTerm} type="text" className="rounded-pill round-search input outline-0 border-0">
 
 						</FormControl>
+
+						{searchTerm ? <span><i className="fa fa-times align-self-end cross"></i></span> : ''}
 					</div>
 					
 					<div className="d-flex justify-content-center my-4">
-						<Button size="lg" variant="light">Google Search</Button>
-						<Button className="ml-2" size="lg" variant="light">I'm Feeling Lucky</Button>  
+						<Button type="submit" size="md" variant="light">Google Search</Button>
+						<Button className="ml-2" size="md" variant="light">I'm Feeling Lucky</Button>  
 					</div>
 
 				</div>
